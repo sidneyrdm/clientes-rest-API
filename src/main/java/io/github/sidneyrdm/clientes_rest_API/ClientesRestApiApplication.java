7365cdd1c9014e0364f9c876fdd011b70abd75c4
sidneyrdm <sidneybritomal@gmail.com>
2025-03-16 11:48:56 -0300

    criação do projeto através do spring.io e importação do projeto na ide InteliJ

---
+++ src/main/java/io/github/sidneyrdm/clientes_rest_API/ClientesRestApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.sidneyrdm.clientes_rest_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/sidneyrdm/clientes_rest_API/ClientesRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sidneyrdm.clientes_rest_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
